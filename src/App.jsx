1ada6f36d603593a1fe4f2407ea4b8b15b2d13b9
natalia mohyla <mogilesha81@gmail.com>
2025-03-24 13:16:09 +0100

    task4

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Statistics from './components/Statistics/Statistics';
 import article from './data/article.json';
 import stats from './data/stats.json';
 import forbes from './data/forbes.json';
+import transactions from './data/transactions.json';
 export const App = () => {
   return (
     <Section>
@@ -32,7 +33,7 @@ export const App = () => {
         <ForbesList list={forbes} />,
 
         <Heading title="Task 4 Crypto history" top bottom />
-        <CryptoHistory />
+        <CryptoHistory items={transactions} />;
       </Container>
     </Section>
   );
